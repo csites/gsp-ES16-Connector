d8dd177ee0b3a560445819dcb54adfc88e08b757
csites <81783717+csites@users.noreply.github.com>
2023-11-19 16:42:51 -0500

    Update ES16parser9.py

--- ES16parser9.py
+++ ES16parser9.py
@@ -130,19 +130,19 @@ while (loop == True):
   if (ser.inWaiting() > 0):
     ser.timeout = 0.3
     try: 
-          # pass 1.  
+          # pass 1.   Read data + carriage return
           data = ser.read(169)
           ser.timeout=0
-          string_data = data.decode('utf-8')
+          string_data = data.decode('utf-8')[:168]
           if (len(string_data) == 168):
              try:
                 # pass 2.  
                 ser.timeout = 0.3  
                 data2 = ser.read(169)
                 ser.timeout=0
-                string_data2 = data.decode('utf-8')
+                string_data2 = data.decode('utf-8')[:168]
                 print(f"data2 lenght = {len(string_data2)}") 
-                if (len(string_data2) == 169):
+                if (len(string_data2) == 168):
                     parsed_data2 = process_input_string(string_data2)
                     if (parsed_data2 != None):
                        print_color_prefix(Color.YELLOW, "||  ES16 SERIAL LINE READ/PARSE  ||","Data recieved in pass2")

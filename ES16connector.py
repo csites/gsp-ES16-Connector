370b3f3210d3c4b3b28f54c9f359fa863a843f91
csites <81783717+csites@users.noreply.github.com>
2023-12-17 17:17:05 -0500

    Update ES16connector.py

--- ES16connector.py
+++ ES16connector.py
@@ -265,7 +265,7 @@ class PuttHandler(BaseHTTPRequestHandler):
 """ 
 ThreadingHTTPServer.  Let see if this helps.   
 """
-class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+class ThreadedSimpleServer(ThreadingMixIn, HTTPServer):
     pass
 
 """
@@ -280,7 +280,7 @@ it looks like '/putting' portion of the url is ignored.
 class PuttServer(threading.Thread):
     def run(self):
         print_color_prefix(Color.GREEN, "Putting Server ||", "Starting. Use ball_tracking from https://github.com/alleexx/cam-putting-py")
-        self.server = ThreadingHTTPServer(('0.0.0.0', 8888), PuttHandler).serve_forever()
+        self.server = ThreadedHTTPServer(('0.0.0.0', 8888), PuttHandler).serve_forever()
         return
  #       self.server.serve_forever()
  #       server_thread = threading.Thread(target=self.server.serve_forever, daemon=False)

54e7611e023945db2dae38f0e75bfa519e10d97c
csites <81783717+csites@users.noreply.github.com>
2023-12-17 17:08:55 -0500

    Update ES16connector.py

--- ES16connector.py
+++ ES16connector.py
@@ -9,6 +9,7 @@ import math
 import re
 from http.server import HTTPServer, BaseHTTPRequestHandler
 # from http.server import ThreadingHTTPServer, import HTTPServer BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
 import threading
 from queue import Queue
 import select

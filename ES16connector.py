0dd9cec53afe3d124a5bbb3af6883e3107eff6ad
csites <81783717+csites@users.noreply.github.com>
2023-12-17 15:14:59 -0500

    Update ES16connector.py

--- ES16connector.py
+++ ES16connector.py
@@ -112,7 +112,7 @@ lock_q = threading.Lock()
 # Establish a shot Queue
 shot_q = Queue()
 # Also make voice global for the putt server 
-global voice = None
+voice = None
 
 # Key/value arrays for club selection routines.
 ES_gsp_Clubs="Drv DR, 3Wd W2, 3Wd W3, 4Wd W4, 5Wd W5, 7Wd W7, 7Wd W6, 2Hy H2, 3Hy H3, 4Hy H4, 5Hy H7, 5Hy H6, 5Hy H5, 2Ir I2, 2Ir I1, 3Ir I3, 4Ir I4, 5Ir I5, 6Ir I6,  7Ir I7, 8Ir I8, 9Ir I9, Ptw PW, Gpw GW, Sdw SW, Ldw LW, Chp LW, Ptt PT"
